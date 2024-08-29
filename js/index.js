26062b88e4752ce2cbbc455505adebc2815e3a44
AleksandrN <alam.mid.oev@gmail.com>
2024-08-30 01:16:26 +0300

    rework move plate logic on insert_before base

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
 const gameField = document.querySelector('#game_field');
 const mixButton = document.querySelector('button.mix_game')
-// let gameArray = [
-//     [[1],[2],[3],[4]],
-//     [[5],[6],[7],[8]],
-//     [[9],[10],[11],[12]],
-//     [[13],[14],[15],[16]]
-// ];
 
 let gameArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 
@@ -14,130 +8,85 @@ mixButton.addEventListener('click', () => {
 })
 
 function mixGame() {
-    // for (let i = 0; i < 4; i++) {
-    //     for (let j = 0; j < 4; j++) {
-    //         let rand1 = Math.floor(Math.random()*4);
-    //         let rand2 = Math.floor(Math.random()*4);
-
-    //         if(rand1 == rand2 & rand1 == 3 || i == j & i == 3) continue            
-    //         swapElement(i,j,rand1,rand2);
-    //     }
-    // }
-
     for (let i = 0; i < 15; i++) {
         let randIndex = Math.floor(Math.random()*15);
 
-        swapElement(i, randIndex)
+        let tmpElement = gameArray[i];
+        gameArray[i] = gameArray[randIndex]
+        gameArray[randIndex] = tmpElement; 
     }
 
     drawGame()
 }
 
-// function swapElement(y_element1,x_element1,y_element2,x_element2) {
-//     let tmpElement = gameArray[y_element1][x_element1];
-//     gameArray[y_element1][x_element1] = gameArray[y_element2][x_element2];
-//     gameArray[y_element2][x_element2] = tmpElement;
-// }
-
-function swapElement(index1,index2) {
-    let tmpElement = gameArray[index1];
-    gameArray[index1] = gameArray[index2]
-    gameArray[index2] = tmpElement; 
-}
-
 function drawGame() {
     let tmpHTML = ""
-    // for (let i = 0; i < 4; i++) {
-    //     for (let j = 0; j < 4; j++) {
-    //         tmpHTML += gameArray[i][j] == 16 ? `<div style="visibility:hidden;">${gameArray[i][j]}</div>` : `<div>${gameArray[i][j]}</div>`;
-    //     }
-    // }
-
     for (let i = 0; i < 16; i++) {
-        tmpHTML += gameArray[i] == 16 ? `<div style="visibility:hidden;" data-value-type="zero">${gameArray[i]}</div>` : `<div>${gameArray[i]}</div>`;
+        tmpHTML += gameArray[i] == 16 ? `<div data-value-index="${i}" data-value-type="empty"></div>` : `<div data-value-index="${i}">${gameArray[i]}</div>`;
     }
     gameField.innerHTML = tmpHTML;
 
     let gamePlate = document.querySelectorAll('#game_field > div')
     gamePlate.forEach((plate)=>{
-        plate.addEventListener('click',() => {
-            movePlate(plate.textContent);
+        plate.addEventListener('click',(e) => {
+            movePlate(e.currentTarget);
         })
     })
 }
-// ------------------------------------------------------
-function movePlate(plateNumber) {
-    let numberCords = getCords(plateNumber);
-    // let zeroCords = getCords(16);
 
-    // if (!clickValidate(numberCords,zeroCords)) return 0;
-    if (!clickValidate(numberCords)) return 0;
-
-    swapElement(numberCords,zeroCords);
-    drawGame();
-    setTimeout(()=>{checkWin()},100)
-}
-
-function getCords(number) {
-    // for (let i = 0; i < 4; i++) {
-    //     for (let j = 0; j < 4; j++) {
-    //         if (gameArray[i][j] == number) return {i,j};
-    //     }
-    // }
-
-    for (let i = 0; i < 16; i++) {
-        if (gameArray[i] == number) return i;
-    }
-}
-
-// function clickValidate(numberCords,zeroCords) {
-//     if (
-//         // Math.max(y_element1,y_element2) - Math.min(y_element1,y_element2) > 1 ||
-//         // Math.max(x_element1,x_element2) - Math.min(x_element1,x_element2) > 1 ||
-//         // x_element1 - x_element2 != 0 && y_element1 - y_element2 != 0
-
-//         Math.max(numberCords,zeroCords) - Math.min(numberCords,zeroCords) == 1 ||
-//         Math.max(numberCords,zeroCords) - Math.min(numberCords,zeroCords) == 4
-//     ) {
-//         return true;
-//     }
-//     return false;
-// }
-
-function clickValidate(clickedPlate) { 
-    let gamePlate = document.querySelectorAll('#game_field > div')
-
-    let rightPlate = gamePlate[clickedPlate].nextElementSibling || null;
-    let leftPlate = gamePlate[clickedPlate].previousElementSibling || null;
-    let bottomPlate = gamePlate[clickedPlate+4] || null;
-    let upPlate = gamePlate[clickedPlate-4] || null;
-
+function movePlate(plate) {
+    let rightPlate = plate.nextElementSibling;
+    let leftPlate = plate.previousElementSibling;
+    let bottomPlate = document.querySelector(`div[data-value-index="${Number(plate.dataset.valueIndex)+4}"]`);
+    let upPlate = document.querySelector(`div[data-value-index="${Number(plate.dataset.valueIndex)-4}"]`);
     
-
-    if (
-        rightPlate && rightPlate.hasAttribute("data-value-type") ||
-        leftPlate && leftPlate.hasAttribute("data-value-type") ||
-        bottomPlate && bottomPlate.hasAttribute("data-value-type") ||
-        upPlate && upPlate.hasAttribute("data-value-type")
-    ) {
-        return true
+    switch (true) {
+        case rightPlate && rightPlate.hasAttribute("data-value-type"):
+            swapElement(plate,rightPlate,'right');
+            break;
+        case leftPlate && leftPlate.hasAttribute("data-value-type"):
+            swapElement(plate,leftPlate,'left');            
+            break;
+        case bottomPlate && bottomPlate.hasAttribute("data-value-type"):
+            swapElement(plate, bottomPlate, 'bottom');
+            break;
+        case upPlate && upPlate.hasAttribute("data-value-type"):
+            swapElement(plate, upPlate, 'top');
+            break;
     }
 
-    return false
-
+    setTimeout(() => checkWin(), 100);
 }
 
+function swapElement(plate1,plate2,side) { 
+    switch (side) {
+        case 'right':
+            gameField.insertBefore(plate2, plate1)
+            break;
+        case 'left':
+            gameField.insertBefore(plate1, plate2)
+            break;
+        case 'top':
+            gameField.insertBefore(plate2,plate1)
+            gameField.insertBefore(plate1,gameField.children[plate2.dataset.valueIndex])
+            break;
+        case 'bottom':
+            gameField.insertBefore(plate1,plate2)
+            gameField.insertBefore(plate2,gameField.children[plate1.dataset.valueIndex])
+            break;
+    }
+
+    let tmpIndex = plate1.dataset.valueIndex;
+    plate1.dataset.valueIndex = plate2.dataset.valueIndex;
+    plate2.dataset.valueIndex = tmpIndex;
+}
 
 function checkWin() {
     let wrongPossition = 0;
-    // for (let i = 0; i < 4; i++) {
-    //     for (let j = 0; j < 4; j++) {
-    //         if (gameArray[i][j] != i*4+j+1) wrongPossition++;
-    //     }
-    // }
 
-    for (let i = 0; i < 16; i++) {
-        if (gameArray[i] != i+1) wrongPossition++;
+    for (let i = 0; i < 15; i++) {
+        let plate = gameField.children[i]
+        if (plate.dataset.valueIndex != Number(plate.textContent)-1) wrongPossition++;
     }
 
     if (wrongPossition == 0) alert('Победа')
